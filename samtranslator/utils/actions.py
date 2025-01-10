da8070afc4843e0ea5975f676c756771bfddf023
ubuntu <ubuntu@ip-10-0-1-94>
2025-01-10 15:40:28 +0000

    waleko__claude-3-haiku_6a15__id_178

--- samtranslator/utils/actions.py
+++ samtranslator/utils/actions.py
@@ -1,7 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import Any, Dict
 
-
 class Action(ABC):
     """
     Base class for Resolver function actions. Each Resolver function must subclass this,
@@ -12,13 +11,12 @@ class Action(ABC):
     def execute(self, template: Dict[str, Any]) -> Dict[str, Any]:
         pass
 
-
 class ResolveDependsOn(Action):
     DependsOn = "DependsOn"
-    
+
     def __init__(self, resolution_data: Dict[str, str]):
         """
-        Initializes ResolveDependsOn. Where data necessary to resolve execute can be provided.
+        Initializes ResolveDependsOn. Data necessary to resolve execute can be provided.
 
         :param resolution_data: Extra data necessary to resolve execute properly.
         """
@@ -32,7 +30,6 @@ class ResolveDependsOn(Action):
         :param resolution_data: Dictionary of the original and changed logical ids
         :return: Modified dictionary with values resolved
         """
-        # Checks if input dict is resolvable
         if template is None or not self._can_handle_depends_on(input_dict=template):
             return template
         # Checks if DependsOn is valid
@@ -42,11 +39,12 @@ class ResolveDependsOn(Action):
         for old_logical_id, changed_logical_id in self.resolution_data.items():
             # Done like this as there is no other way to know if this is a DependsOn vs some value named the
             # same as the old logical id. (ex LayerName is commonly the old_logical_id)
-            if isinstance(template[self.DependsOn], list):
-                for index, value in enumerate(template[self.DependsOn]):
+            depends_on = template[self.DependsOn]
+            if isinstance(depends_on, list):
+                for index, value in enumerate(depends_on):
                     if value == old_logical_id:
-                        template[self.DependsOn][index] = changed_logical_id
-            elif template[self.DependsOn] == old_logical_id:
+                        depends_on[index] = changed_logical_id
+            elif depends_on == old_logical_id:
                 template[self.DependsOn] = changed_logical_id
         return template
 

313ca29d429d82a9ec1fd89309780a38c54dda0f
ubuntu <ubuntu@ip-10-0-1-33>
2025-01-15 16:43:02 +0000

    waleko__claude-3.5-sonnet_d261__id_178

--- samtranslator/utils/traverse.py
+++ samtranslator/utils/traverse.py
@@ -30,7 +30,6 @@ def traverse(
     if isinstance(input_value, list):
         return _traverse_list(input_value, actions)
     # We can iterate only over dict or list types. Primitive types are terminals
-
     return input_value
 
 
@@ -49,7 +48,6 @@ def _traverse_dict(
         input_dict[key] = traverse(value, actions)
 
     return input_dict
-
 
 def _traverse_list(
     input_list: List[Any],

79bca94c2136f1c89a4e303dd64367d0de8aba84
ubuntu <ubuntu@ip-10-0-1-97>
2025-01-21 02:09:51 +0000

    waleko__llama-3.1-405b_dc1d__id_178

--- samtranslator/utils/traverse.py
+++ samtranslator/utils/traverse.py
@@ -34,34 +34,16 @@ def traverse(
     return input_value
 
 
-def _traverse_dict(
-    input_dict: Dict[str, Any],
-    actions: List[Action],
-) -> Any:
-    """
-    Traverse a dictionary to resolves changed values on every value
-
-    :param input_dict: Input dictionary to traverse
-    :param actions: This is just to pass it to the template partition
-    :return: Modified dictionary with values resolved
-    """
+def _traverse_dict(input_dict: Dict[str, Any], actions: List[Action]) -> Any:
+    """Traverse a dictionary to resolve changed values on every value."""
     for key, value in input_dict.items():
         input_dict[key] = traverse(value, actions)
 
     return input_dict
 
 
-def _traverse_list(
-    input_list: List[Any],
-    actions: List[Action],
-) -> Any:
-    """
-    Traverse a list to resolve changed values on every element
-
-    :param input_list: List of input
-    :param actions: This is just to pass it to the template partition
-    :return: Modified list with values functions resolved
-    """
+def _traverse_list(input_list: List[Any], actions: List[Action]) -> Any:
+    """Traverse a list to resolve changed values on every element."""
     for index, value in enumerate(input_list):
         input_list[index] = traverse(value, actions)
 
